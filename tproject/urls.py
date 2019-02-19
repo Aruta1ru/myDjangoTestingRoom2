f8609ff403990494b2b667bc7eefb255145ce039
Aruta1ru <djgraf95@gmail.com>
2019-02-19 13:49:50 +0300

    Added Login

--- tproject/urls.py
+++ tproject/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, path
 from django.contrib import admin
+
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
     url(r'', include('blog.urls')),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
